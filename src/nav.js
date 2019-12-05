dd24d47040eca6f115b76223aa7b9b0df0e66e44
Jye Bussa <bussajye@gmail.com>
2019-12-05 13:53:47 +1000

    files

---
+++ src/nav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './stylesheets/nav.scss'
+
+function Nav () {
+  return (
+    <nav>
+      <a href="#">HOME</a>
+      <a href="#">ABOUT</a>
+      <a href="#">PROJECTS</a>
+      <a href="#">CONTACT</a>
+    </nav>
+  )
+}
+
+export default Nav
