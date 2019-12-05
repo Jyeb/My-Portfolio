3234686b11b8a1ffb2385f2aec3cbab264963ef0
Jye Bussa <bussajye@gmail.com>
2019-12-05 21:47:17 +1000

    minor changes'

--- src/about.js
+++ src/about.js
@@ -12,13 +12,20 @@ function About () {
           machine learning, and mathematics. I have previously, worked in the engineering industry as a structural draftsman, with experience meeting strict deadlines, and a professional work environment.
           I have worked with the technologies listed below.
   `    </p>
-        <ul>
+        <ul className="skills-grid">
+          <img className="ruby-logo" src="#" alt="ruby language logo" />
           <li>Ruby</li>
+          <img className="rails-logo" src="#" alt="rails language logo" />
           <li>Ruby on Rails</li>
+          <img className="python-logo" src="#" alt="python language logo" />
           <li>Python</li>
+          <img className="javascript-logo" src="#" alt="javascript language logo" />
           <li>JavaScript</li>
+          <img className="react-logo" src="#" alt="react language logo" />
           <li>React</li>
+          <img className="html-logo" src="#" alt="html language logo" />
           <li>HTML</li>
+          <img className="css-logo" src="#" alt="css language logo" />
           <li>CSS</li>
         </ul>
       </section>
--- src/projects.js
+++ src/projects.js
@@ -8,7 +8,7 @@ function Projects () {
       <p>Below is a list of the projects I have completed.</p>
       <p>Terminal application created for the week 3 assessment for Coder Academy.</p>
       <p>Two-sided marketplace application completed for the third assessment of Coder Academy</p>
-      <p>Ongoing project building CRM software for a real estate company.</p>
+      <p>Ongoing project building CRM software for a client in the real estate industry.</p>
     </div> 
   )
 }
