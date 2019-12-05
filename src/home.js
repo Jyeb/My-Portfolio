75f5f3dbd92370934f3766056c730e047efbe4f2
Jye Bussa <bussajye@gmail.com>
2019-12-05 19:59:56 +1000

    homepage styling

--- src/home.js
+++ src/home.js
@@ -1,14 +1,14 @@
 import React, { Fragment } from 'react'
 import './stylesheets/home.scss'
-
 function Home () {
   return (
     <Fragment>
-      <div className="background"></div>
-      <div>
+      <img src="/images/background.jpg" alt="background image" />
+      <div className="homeinfo">
         <h1>Hello, I'm Jye Bussa</h1>
         <p>Student</p>
         <p>Software Developer</p>
+        <a href="/about">ABOUT</a>
       </div>
     </Fragment>
   )
--- src/nav.js
+++ src/nav.js
@@ -4,10 +4,12 @@ import './stylesheets/nav.scss'
 function Nav () {
   return (
     <nav>
-      <a href="home">HOME</a>
+      <div className="nav-options">
+      <a href="/">HOME</a>
       <a href="about">ABOUT</a>
       <a href="projects">PROJECTS</a>
       <a href="contact">CONTACT</a>
+      </div>
     </nav>
   )
 }
