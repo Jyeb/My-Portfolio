93f9ee3ace4fe713a1a538cc43bdb1367896b20b
Jye Bussa <bussajye@gmail.com>
2019-12-07 19:21:24 +1000

    projects page

--- src/projects.js
+++ src/projects.js
@@ -5,10 +5,23 @@ function Projects () {
   return (
     <section className="projects-section">
       <h1>PROJECTS</h1>
-      <p>Below is a list of the projects I have completed.</p>
-      <p>Terminal application created for the week 3 assessment for Coder Academy.</p>
-      <p>Two-sided marketplace application completed for the third assessment of Coder Academy</p>
-      <p>Ongoing project building Custom software for a client in the real estate industry.</p>
+      <p>Below are some of my latest projects.</p>
+      <div className="projects-grid">         
+        <div className="container">
+          <img className="battleships" src="/images/Battleships.jpeg" alt="battleship game screenshot" />
+          <div className="overlay">
+            <div className="text">Terminal application created for the week 3 assessment for Coder Academy.</div>
+          </div>
+        </div>
+        
+        <div className="container">
+          <img className="marketplace" src="/images/two-sided-marketplace.png" alt="marketplace application screenshot" />
+          <div className="overlay">
+            <div className="text">Two-sided marketplace application completed for the third assessment of Coder Academy.</div>
+          </div>
+        </div>
+      </div>
+      {/* <p>Ongoing project building Custom software for a client in the real estate industry.</p> */}
     </section> 
   )
 }
