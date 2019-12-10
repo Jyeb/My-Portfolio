5fa3fadd5f7e25fdf5a5a09dbba909810f5d85b3
Jye Bussa <bussajye@gmail.com>
2019-12-10 14:57:28 +1000

    images

--- src/about.js
+++ src/about.js
@@ -23,39 +23,39 @@ function About () {
           <h1>SKILLS</h1>
         <ul className="skills-grid">
         <li>
-          <img className="ruby-logo" src="/images/ruby.svg" alt="ruby language logo" /><br />
+          <img className="ruby-logo" src={`${process.env.PUBLIC_URL}/images/ruby.svg`} alt="ruby language logo" /><br />
           Ruby
         </li>
         <li>
-          <img className="rails-logo" src="/images/rails.svg" alt="rails language logo" /><br />
+          <img className="rails-logo" src={`${process.env.PUBLIC_URL}/images/rails.svg`} alt="rails language logo" /><br />
           Ruby on Rails
         </li>
         <li>
-          <img className="python-logo" src="/images/python.svg" alt="python language logo" /><br />
+          <img className="python-logo" src={`${process.env.PUBLIC_URL}/images/python.svg`} alt="python language logo" /><br />
           Python   
         </li>
         <li>
-          <img className="javascript-logo" src="/images/javascript.svg" alt="javascript language logo" /><br />
+          <img className="javascript-logo" src={`${process.env.PUBLIC_URL}/images/javascript.svg`} alt="javascript language logo" /><br />
           JavaScript
         </li>
         <li>
-          <img className="react-logo" src="/images/react.svg" alt="react language logo" /><br />
+          <img className="react-logo" src={`${process.env.PUBLIC_URL}/images/react.svg`} alt="react language logo" /><br />
           React
         </li>
         <li>
-          <img className="html-logo" src="/images/html.svg" alt="html language logo" /><br />
+          <img className="html-logo" src={`${process.env.PUBLIC_URL}/images/html.svg`} alt="html language logo" /><br />
           HTML
         </li>
         <li>
-          <img className="css-logo" src="/images/css.svg" alt="css language logo" /><br />
+          <img className="css-logo" src={`${process.env.PUBLIC_URL}/images/css.svg`} alt="css language logo" /><br />
           CSS
         </li>
         <li>
-          <img className="css-logo" src="/images/git.png" alt="css language logo" /><br />
+          <img className="css-logo" src={`${process.env.PUBLIC_URL}/images/git.png`} alt="css language logo" /><br />
           Git
         </li>
         <li>
-          <img className="css-logo" src="/images/ubuntu.png" alt="css language logo" /><br />
+          <img className="css-logo" src={`${process.env.PUBLIC_URL}/images/ubuntu.png`} alt="css language logo" /><br />
           Ubuntu
         </li>
         </ul>
