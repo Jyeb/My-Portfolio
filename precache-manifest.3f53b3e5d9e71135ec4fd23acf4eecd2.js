a2eb173d199dde1b4bbc0f3c5b24b03bf32a14f1
Jye Bussa <bussajye@gmail.com>
2019-12-10 11:52:11 +1000

    Updates

---
+++ precache-manifest.3f53b3e5d9e71135ec4fd23acf4eecd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff9c3582a0700d524f6b4b212ca5c4d7",
+    "url": "/My-Portfolio/index.html"
+  },
+  {
+    "revision": "f63c2d936d7a1b6dedb9",
+    "url": "/My-Portfolio/static/css/main.f4c54f76.chunk.css"
+  },
+  {
+    "revision": "7fdd2821e0e21ccfe3f3",
+    "url": "/My-Portfolio/static/js/2.b6284079.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/My-Portfolio/static/js/2.b6284079.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f63c2d936d7a1b6dedb9",
+    "url": "/My-Portfolio/static/js/main.3f7c009d.chunk.js"
+  },
+  {
+    "revision": "1977a55098a58bc30876",
+    "url": "/My-Portfolio/static/js/runtime-main.da466f7f.js"
+  }
+]);
